d81148f5c4fdcc094cfa5c707f46e260e81951f2
Amidou DIAGANA <diaganaamidou@gmail.com>
2026-09-23 15:18:42 +0000

    Revert "Initialisation flutter"
    This reverts commit 42327ebbb12e5ca6849874b52d2474f0fb2e0ce0.

--- IdeaProjects/main/psyavocat_front/android/app/src/main/kotlin/com/example/psyavocat_front/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.psyavocat_front
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- IdeaProjects/main/psyavocat_front/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:psyavocat_front/core/config/app_config.dart';
-
-void main() {
-  test('AppConfig smoke test', () {
-    expect(AppConfig.appName, equals('PsyAvocat'));
-    expect(AppConfig.apiBaseUrl, isNotEmpty);
-  });
-}
